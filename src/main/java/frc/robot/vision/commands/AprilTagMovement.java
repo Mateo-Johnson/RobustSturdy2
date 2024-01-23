212e2554c0d062785bc5859cdd5d41d0db66f18e
Mateo Johnson <johnson.12@themetroschool.org>
2024-01-23 16:32:18 -0500

    silly pt 2

--- src/main/java/frc/robot/vision/commands/AprilTagMovement.java
+++ src/main/java/frc/robot/vision/commands/AprilTagMovement.java
@@ -4,54 +4,66 @@
 
 package frc.robot.vision.commands;
 
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.drivetrain.DriveSubsystem;
-import frc.robot.vision.Vision;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class AprilTagMovement extends CommandBase {
   private final DriveSubsystem driveSubsystem;
-  public static double headingAtThisVeryMomentInTimeAndTwoDimensionalSpace;
-  /** Creates a new AprilTag. */
+  public static double initialHeading;
+  /** Creates a new TurnToAngle. */
   public AprilTagMovement(DriveSubsystem driveSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.driveSubsystem = driveSubsystem;
     addRequirements(driveSubsystem);
-    headingAtThisVeryMomentInTimeAndTwoDimensionalSpace = driveSubsystem.getHeading();
+    initialHeading = driveSubsystem.getHeading();
   }
 
+  //PID VALUES
   double tP = 0.01145;
   double tI = 0.0000176;
   double tD = 0.00098;
-  PIDController aprilTagPID = new PIDController(tP, tI, tD);
-  
+  //CREATE A PID CONTROLLER WITH THE SPECIFIED CONSTANTS
+  PIDController turningPID = new PIDController(tP, tI, tD);
+
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+  double tX = tx.getDouble(0.0);
+  double y = ty.getDouble(0.0);
+  double area = ta.getDouble(0.0);
+
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    SmartDashboard.putNumber("tX", tX);
+    initialHeading = driveSubsystem.getHeading();
+    SmartDashboard.putNumber("Heading", initialHeading);
+    double turnValue = turningPID.calculate(tX, 0);
+    SmartDashboard.putNumber("TurnValue", turnValue);
+    driveSubsystem.drive(0, 0, -turnValue, false, true);
 
-    headingAtThisVeryMomentInTimeAndTwoDimensionalSpace = driveSubsystem.getHeading();
-    double tX = Vision.tX; //THE X OFFSET OF THE TARGET, IN DEGREES FROM -30° TO 30°
-
-    if (Vision.tV == true) { //CHECK IF IT HAS THE TARGET
-
-      //THE LINE BELOW BASICALLY MEANS THAT IT IS CALCULATING THE PID CONTROLLER VALUE, TRYING TO MAKE THE FIRST VALUE MATCH THE SECOND VALUE
-      double turnValue = aprilTagPID.calculate(tX, 0); //CREATE THE PID CONTROLLER, FROM THE STARTING POINT OF THE X OFFSET, AND MOVING TO ZERO
-      SmartDashboard.putNumber("TurnValue", turnValue);
-      driveSubsystem.drive(0, 0, -turnValue, false, true);
-      
-    } 
 
   }
 
+
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+    driveSubsystem.setWheelsX();
+
+  }
 
   // Returns true when the command should end.
   @Override
